ffb6a9e9be3a5c8a950f5beb24004eb547966c6a
Nikko Dacara <franzdacara25@gmail.com>
2021-12-21 19:11:18 +0800

    Bg

--- js/script.js
+++ js/script.js
@@ -35,5 +35,8 @@ menuTabs.addEventListener("click",function(e){
         const target = e.target.getAttribute("data-target");
         menuTabs.querySelector(".active").classList.remove("active");
         e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
     }
 })
